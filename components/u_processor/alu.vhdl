6b68124fb97af6362607d31c9aa0e9de95090ed1
HenriqueZ18 <Hique.rz03@gmail.com>
2023-11-12 16:40:02 -0300

    adapted the alu to receive 8 bit operands instead of 16 bit ones.

--- components/u_processor/alu.vhdl
+++ components/u_processor/alu.vhdl
@@ -4,33 +4,32 @@ use IEEE.numeric_std.all;
 
 entity alu is
     port(
-        op0, op1: in unsigned(15 downto 0);
+        op0, op1: in unsigned(7 downto 0);
         alu_op: in unsigned(2 downto 0);
-        result: out unsigned(15 downto 0);
+        result: out unsigned(7 downto 0);
         n,v,z,c: out std_logic
     );
 end alu;
 
 -- OPERATIONS:
 -- Arithmetic: addition, subtraction;
--- Boolean: greater than, smaller than, equals;
 -- Logical: and, or, xor.
 
 architecture alu_arch of alu is
     
     --<operation>_r(esult)
-    signal sum_r: unsigned(16 downto 0) := (others => '0');
-    signal sub_r: unsigned(15 downto 0) := x"0000";
-    signal and_r: unsigned(15 downto 0) := x"0000";
-    signal or_r: unsigned(15 downto 0) := x"0000";
-    signal xor_r: unsigned(15 downto 0) := x"0000";
+    signal sum_r: unsigned(8 downto 0) := (others => '0');
+    signal sub_r: unsigned(8 downto 0) := (others => '0');
+    signal and_r: unsigned(7 downto 0) := x"00";
+    signal or_r: unsigned(7 downto 0) := x"00";
+    signal xor_r: unsigned(7 downto 0) := x"00";
 
-    signal mux_out: unsigned(15 downto 0) := x"0000";
+    signal mux_out: unsigned(7 downto 0) := x"00";
 
 begin
     -- sum and sub
     sum_r <= ('0'& op0) + ('0'& op1);
-    sub_r <= op0 - op1;
+    sub_r <= ('0'& op0) - ('0'& op1);
 
     -- logical
     and_r <= op0 and op1;
@@ -38,18 +37,18 @@ begin
     xor_r <= op0 xor op1;
 
     -- muxing
-    mux_out <= sum_r(15 downto 0) when alu_op = "000" else
-               sub_r when alu_op = "001" else
+    mux_out <= sum_r(7 downto 0) when alu_op = "000" else
+               sub_r(7 downto 0) when alu_op = "001" else
                and_r when alu_op = "010" else
                or_r when alu_op = "011" else
                xor_r when alu_op = "100" else
-               x"0000";
+               x"00";
 
     --flags
     z <= '1' when mux_out = 0 else '0';
-    v <= '1' when sum_r(16) = '1' else '0';
-    c <= '1' when sum_r(16) = '1' else '0';
-    n <= '1' when mux_out(15) = '1' else '0';
+    v <= '1' when sum_r(8) = '1' else '0';
+    c <= '1' when sum_r(8) = '1' else '0';
+    n <= '1' when mux_out(7) = '1' else '0';
 
     result <= mux_out;
 
