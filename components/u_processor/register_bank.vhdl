6aa8e09e2f90e73de41d6b68f7e4bd8c3d1aab2a
HenriqueZ18 <Hique.rz03@gmail.com>
2023-11-12 16:33:58 -0300

    converted 16 bit registers to 8 bit on register_bank.

--- components/u_processor/register_bank.vhdl
+++ components/u_processor/register_bank.vhdl
@@ -6,30 +6,28 @@ entity register_bank is
     port (
         reg_a_ad, reg_b_ad, write_ad : in unsigned(4 downto 0);
         write_en, rst, clk : in std_logic;
-        write_data : in unsigned(15 downto 0);
-        reg_a, reg_b : out unsigned(15 downto 0)
+        write_data : in unsigned(7 downto 0);
+        reg_a, reg_b : out unsigned(7 downto 0)
     );
 end register_bank;
 
 
 architecture a_register_bank of register_bank is
 
-    type reg_array is array (31 downto 0) of unsigned(15 downto 0);
-    signal reg_file : reg_array;
+    type reg_array is array (31 downto 0) of unsigned(7 downto 0);
+    signal reg_file : reg_array := (others => x"00");
 
 begin
     process (clk, rst)
     begin
         if rst = '1' then
-            reg_file <= (others => (others => '0'));
+            reg_file <= (others => x"00");
         elsif rising_edge(clk) then
-            if write_en = '1' then
-                reg_file(to_integer(unsigned(write_ad))) <= write_data;
+            if (write_en = '1' and not(write_ad = "00000")) then
+                reg_file(to_integer(write_ad)) <= write_data;
             end if;
         end if;
     end process;
-    reg_file(0) <= x"0000";
     reg_a <= reg_file(to_integer(reg_a_ad));
     reg_b <= reg_file(to_integer(reg_b_ad));
-    
 end a_register_bank; 
