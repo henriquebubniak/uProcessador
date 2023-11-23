4fff8df92282d61b672a8788b52d821116e854dd
Henrique Bubniak <henriquebubniak@MacBook-Air-de-Henrique.local>
2023-11-22 23:28:43 -0300

    buba - adjusted program in rom

--- misc/eratosthenes_bz.asm
+++ misc/eratosthenes_bz.asm
@@ -9,6 +9,7 @@ initialize
         lda #2 ; A = 2
         sta 2 ; X = 2
 outer_loop
+        clc
         lda 10,X ; A = ram[10+X]
         cmp #0 ; compare A to 0
         beq next_number ; if A = 0, A is not prime, so go to the next number
@@ -21,6 +22,7 @@ inner_loop
         cpy #50 ; TODO compare Y to 50
         bpl next_number ; if Y is bigger than 50 go to next number
         lda #0 ; A = 0
+        clc
         sta 10,Y ; ram[10+Y] = 0, marked as not prime
         tya ; A = Y
         jmp inner_loop
